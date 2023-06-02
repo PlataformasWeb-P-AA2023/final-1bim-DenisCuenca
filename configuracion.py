c609c338bb4c28f51873324a62553a6c2f850746
DenisCuenca <geraldjt.8@gmail.com>
2023-06-02 07:38:40 -0500

    modificado: datos1.py

--- configuracion.py
+++ configuracion.py
@@ -2,4 +2,6 @@
 # Mysql
 # para el uso de este dialecto en SqlAlchemy
 # instalar "pip install PyMySQL"
-cadena_base_datos = 'mysql+pymysql://usuario:clave@localhost/final'
+# cadena_base_datos = 'mysql+pymysql://root:@localhost/final'
+cadena_base_datos = "sqlite:///final2.db"
+
